2361c5fee166ea0e2c33d2de2ebbe06f2c03b63c
Vchastinet <victorchastinet@live.com>
2017-09-05 16:02:15 -0300

    unified the functions, the same set of functions now work on both rollers

--- js/Functions.js
+++ js/Functions.js
@@ -1,8 +1,6 @@
 
 function singleRoll(dice, bonus) {
 	let result = Math.floor(Math.random() * dice) + 1;
-	addBonus(bonus, result);
-	totalResult(bonus, result);
 	return result;
 }
 
@@ -12,14 +10,13 @@ function multiRoll(dice, n, bonus) {
 		arr.push(singleRoll(dice));
 	}
 	let result = arr;
-	addBonus(bonus, result.join("⦁"));
-	let sumResult = result.reduce((acc, curr) => acc + curr);
-  totalResult(bonus, sumResult);
-  console.log(result);
 	return result;
 }
 
-function addBonus(bonus, result) {
+function partialResult(bonus, result) {
+	if(Array.isArray(result)){
+		result = result.join("⦁")
+	}
 	if (bonus < 1) {
 		$("#result-box").html(result);
 	} else {
@@ -28,31 +25,47 @@ function addBonus(bonus, result) {
 }
 
 function totalResult(bonus, result) {
+	if(Array.isArray(result)){
+		result = result.reduce((acc, curr) => acc + curr);
+	};
 	let total = result + bonus;
 	setTimeout(()=>$("#total-box").html(total), 200);
 }
 
-function advRoll(bonus, advCheck) {
-	let result = multiRoll(20, 2, bonus);
-	
+function advRoll(bonus, advCheck, result) {
 	if (advCheck == "adv") {
 		result = Math.max(result[0], result[1]);
 	} else if (advCheck == "disadv") {
 		result = Math.min(result[0], result[1]);
-	}    
-  totalResult(bonus, result);
-  console.log(result);
-  return result
+	} 
+  return result;
 }
 
-function roller(bonus, advCheck) {
+function roller(bonus, advCheck, dice=20, n) {
 
-	let dice = 20;
+	let result = null;
+
 	//jogada com vantagem/disvantagem
 	if (advCheck != undefined) {
-		return advRoll(bonus, advCheck);
+
+		let partResult = multiRoll(20, 2, bonus);
+		result = advRoll(bonus, advCheck, partResult);
+
+		partialResult(bonus, partResult);
+		totalResult(bonus, result);
+
+		return result
+
 		//jogando apenas 1 dado
-	} else {
-		return singleRoll(dice, bonus);
+	} else if(n > 1){
+		result = multiRoll(dice, n, bonus);
+		//jogando vários dados
+	}	else {
+		result = singleRoll(dice, bonus);
 	}
+
+	partialResult(bonus, result);
+	totalResult(bonus, result);
+
+	return result;
 }
--- js/roller.js
+++ js/roller.js
@@ -1,65 +1,6 @@
-function singleRoll(dice, bonus) {
-	let result = Math.floor(Math.random() * dice) + 1;
-	addBonus(bonus, result);
-	totalResult(bonus, result);
-	return result;
-}
-
-function multiRoll(dice, n, bonus) {
-	var arr = [];
-	for (var i = 0; i < n; i++) {
-		arr.push(singleRoll(dice));
-	}
-	let result = arr;
-	addBonus(bonus, result.join("⦁"));
-	let sumResult = result.reduce((acc, curr) => acc + curr);
-	totalResult(bonus, sumResult);
-	return result;
-}
-
-function advRoll(bonus, advantage, disadvantage) {
-	let result = multiRoll(20, 2, bonus);
-	
-	if (advantage) {
-		result = Math.max(result[0], result[1]);
-	} else if (disadvantage) {
-		result = Math.min(result[0], result[1]);
-	}
-	
-	totalResult(bonus, result);
-}
-
-function addBonus(bonus, result) {
-	if (bonus < 1) {
-		$("#resultBox").html(result);
-	} else {
-		$("#resultBox").html(`${result} (+${bonus})`);
-	}
-}
-
-function totalResult(bonus, result) {
-	let total = result + bonus;
-	setTimeout(()=>$("#totalBox").html(total), 200);
-}
-
-function roller() {
-	var n = Number($("#quant").val());
-	var dice = Number($("#faces").val());
-	var bonus = Number($("#bonus").val());
-	var adv = $("#advantage:checked").val();
-	var disadv = $("#disadvantage:checked").val();
-	
-	//jogada com vantagem/disvantagem
-	if (adv || disadv) {
-		advRoll(bonus, adv, disadv);
-		//jogando apenas 1 dado
-	} else if (n < 2) {
-		singleRoll(dice, bonus);
-		//jogando 2 ou mais dados
-	} else {
-		multiRoll(dice, n, bonus);
-	}
-}
+let n = $("#quant");
+let dice = $("#faces");
+let bonus = $("#bonus");
 
 $("#advantage").on("click", function() {
 	$(".quant").toggle("slow");
@@ -81,5 +22,6 @@ $("#button").mouseout(function (){
 });
 
 $("#button").on("click", function() {
-	roller();
+	let advCheck = $("input:checked").val();
+	roller(Number(bonus.val()), advCheck, dice.val(), n.val());
 });
