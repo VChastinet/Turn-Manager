67c41405f391f0b6e39c2ca7ff874b9801aa1563
Vchastinet <victorchastinet@live.com>
2017-09-11 23:20:34 -0300

    fixing the 'death' icon

--- js/index.js
+++ js/index.js
@@ -49,7 +49,7 @@ $("[value='disadv']").change(function() {
 function killSwitch(element){
       
   if ($(element).text() == "Kill"){
-    $(element).html('<img src="../death.png">')
+    $(element).html('<img src="https://vchastinet.github.io/Turn-Manager/death.png">')
     $(element).closest("tr").attr("id", "dead");
   } else{
     $(element).html('<button class="btn waves-effect waves-light orange">Kill</button>')
