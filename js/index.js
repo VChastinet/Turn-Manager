ce95924271b6b83e1d4b3cf998f12a1bacf0b366
Vchastinet <victorchastinet@live.com>
2017-09-11 23:10:43 -0300

    improved responsive design, working better in small screens. The re-roller still not work

--- js/index.js
+++ js/index.js
@@ -1,42 +1,58 @@
+function applyModal(){
+  if (screen.width > 1400) {
+    $("#re-roller").removeClass("modal");
+    $("#re-roller div:first-child").removeClass("modal-content");
+  } else{
+    $("#re-roller").addClass("modal");
+    $("#re-roller div:first-child").addClass("modal-content");
+  }
+}
+
+$(function(){
+  $('.modal').modal();
+  $("select").material_select();
+  applyModal();
+  $(window).resize(applyModal);
+}); 
+
+
 let managerController = new ManagerController();
 
-  $("select").material_select();
-  
-  $("#add-char").click(()=>$(".char-form").stop().slideToggle());
-  
-  $("#save-party").click(() => {
-    managerController.saveParty();
-    alert("The party is resting in the tavern.");
-  });
-  
-  $("#load-party").click(() => {
-    managerController.loadParty();
-    alert("let the party start.");
-  });
+$("#add-char").click(()=>$(".char-form").stop().slideToggle());
 
-  $("[value='adv']").change(function() {
-      if(this.checked){
-       $(this).closest("div").find("[value='disadv']").prop("disabled", true); 
-      } else {
-        $(this).closest("div").find("[value='disadv']").prop("disabled", false);
-      }
-  });
+$("#save-party").click(() => {
+  managerController.saveParty();
+  alert("The party is resting in the tavern.");
+});
 
-  $("[value='disadv']").change(function() {
+$("#load-party").click(() => {
+  managerController.loadParty();
+  alert("let the party start.");
+});
+
+$("[value='adv']").change(function() {
     if(this.checked){
-      $(this).closest("div").find("[value='adv']").prop("disabled", true); 
+      $(this).closest("div").find("[value='disadv']").prop("disabled", true); 
     } else {
-      $(this).closest("div").find("[value='adv']").prop("disabled", false);
+      $(this).closest("div").find("[value='disadv']").prop("disabled", false);
     }
-  });
-    
-  function killSwitch(element){
-       
-    if ($(element).text() == "Kill"){
-      $(element).html('<img src="../death.png">')
-      $(element).closest("tr").attr("id", "dead");
-    } else{
-      $(element).html('<button class="btn waves-effect waves-light orange">Kill</button>')
-      $(element).closest("tr").attr("id", "");
-    }
+});
+
+$("[value='disadv']").change(function() {
+  if(this.checked){
+    $(this).closest("div").find("[value='adv']").prop("disabled", true); 
+  } else {
+    $(this).closest("div").find("[value='adv']").prop("disabled", false);
   }
+});
+  
+function killSwitch(element){
+      
+  if ($(element).text() == "Kill"){
+    $(element).html('<img src="../death.png">')
+    $(element).closest("tr").attr("id", "dead");
+  } else{
+    $(element).html('<button class="btn waves-effect waves-light orange">Kill</button>')
+    $(element).closest("tr").attr("id", "");
+  }
+}
